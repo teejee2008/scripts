d8c103f618f31c0f5061a7c6a2c6a6d2047a7d4c
Tony George <teejeetech@gmail.com>
2020-09-05 11:55:24 +0530

    Update install-refind-theme-regular-dark.sh

--- install-refind-theme-regular-dark.sh
+++ install-refind-theme-regular-dark.sh
@@ -2,18 +2,24 @@
 
 cd "$(mktemp -d)"
 
+echo ""
 echo "Install git..."
 sudo apt install refind git tree
 
+echo ""
 echo "Download theme files..."
 git clone --depth 1 https://github.com/bobafetthotmail/refind-theme-regular.git || exit 1
 echo "OK"
 
+echo ""
 echo "Install theme files..."
 sudo rm -rf /boot/efi/EFI/refind/{regular-theme,refind-theme-regular}
 sudo cp -r refind-theme-regular /boot/efi/EFI/refind/
 
+echo ""
+echo "Apply theme..."
 conf="/boot/efi/EFI/refind/refind.conf"
-if ! [ grep -q 'include refind-theme-regular/theme.conf' "$conf" ]; then
+if ! grep -q 'include refind-theme-regular/theme.conf' "$conf" ; then
 echo "include refind-theme-regular/theme.conf" | sudo tee -a "$conf"
 fi
+
