e65b4bf4e8dc29c8d17d6f6302b785602444a010
Tony George <teejeetech@gmail.com>
2020-09-05 12:34:23 +0530

    Update install-refind-theme-regular-light.sh

--- install-refind-theme-regular-light.sh
+++ install-refind-theme-regular-light.sh
@@ -26,15 +26,3 @@ conf="/boot/efi/EFI/refind/refind.conf"
 if ! sudo grep -q 'include refind-theme-regular/theme.conf' "$conf" ; then
 echo "include refind-theme-regular/theme.conf" | sudo tee -a "$conf"
 fi
-
-echo ""
-echo "Apply dark theme..."
-echo ""
-conf="/boot/efi/EFI/refind/refind-theme-regular/theme.conf"
-sudo sed -E -i 's/^(banner refind-theme-regular.*)/#\1/g' "$conf"
-sudo sed -E -i 's/^#(banner refind-theme-regular\/icons\/128-48\/bg_dark.png)/\1/g' "$conf"
-sudo sed -E -i 's/^(selection_big refind-theme-regular.*)/#\1/g' "$conf"
-sudo sed -E -i 's/^#(selection_big refind-theme-regular\/icons\/128-48\/selection_dark-big.png)/\1/g' "$conf"
-sudo sed -E -i 's/^(selection_small refind-theme-regular.*)/#\1/g' "$conf"
-sudo sed -E -i 's/^#(selection_small refind-theme-regular\/icons\/128-48\/selection_dark-small.png)/\1/g' "$conf"
-
