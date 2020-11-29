4a3e114c1fb0e34311a28a53d995b7a00160a450
Tony George <teejeetech@gmail.com>
2020-11-29 21:50:08 +0530

    Update install-refind-theme-regular-light.sh
    Check for refind.conf before installing

--- install-refind-theme-regular-light.sh
+++ install-refind-theme-regular-light.sh
@@ -16,13 +16,28 @@ echo "OK"
 echo ""
 echo "Install theme files..."
 echo ""
-sudo rm -rf /boot/efi/EFI/refind/{regular-theme,refind-theme-regular}
-sudo cp -r refind-theme-regular /boot/efi/EFI/refind/
+
+refind_path=""
+
+if sudo test -f /boot/efi/EFI/refind/refind.conf ; then
+    refind_path=/boot/efi/EFI/refind
+elif sudo test -f /boot/efi/EFI/BOOT/refind.conf ; then
+    refind_path=/boot/efi/EFI/BOOT
+else
+    echo "Refind was not found on this system"
+    exit 1
+fi
+
+echo "Found Refind in path: ${refind_path}"
+
+sudo rm -rf "${refind_path}/{regular-theme,refind-theme-regular}"
+sudo cp -r refind-theme-regular "${refind_path}/"
 
 echo ""
 echo "Apply theme..."
 echo ""
-conf="/boot/efi/EFI/refind/refind.conf"
+
+conf="${refind_path}/refind.conf"
 if ! sudo grep -q 'include refind-theme-regular/theme.conf' "$conf" ; then
-echo "include refind-theme-regular/theme.conf" | sudo tee -a "$conf"
+    echo "include refind-theme-regular/theme.conf" | sudo tee -a "$conf"
 fi
